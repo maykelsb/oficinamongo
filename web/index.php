d2449bc81624b2f99b4cef270e185a8ddcec17cc
Maykel Santos Braz <MaykelBraz@mec.gov.br>
2016-04-25 14:13:13 -0300

    Modal de criação de posts

--- web/index.php
+++ web/index.php
@@ -9,15 +9,13 @@ $twig = new Twig_Environment($twigLoader, array(
     //'cache' => '../temp/'
 ));
 
-$db = Db::getInstance();
+#$db = Db::getInstance();
 
-$query = new MongoDB\Driver\Query([]);
-
-echo '<pre>';
-foreach ($db->query($query, 'about') as $r) {
-	print_r($r);
-}
-echo '</pre>';
-
+#$query = new MongoDB\Driver\Query([]);
+#echo '<pre>';
+#foreach ($db->query($query, 'about') as $r) {
+#	print_r($r);
+#}
+#echo '</pre>';
 
 echo $twig->loadTemplate('posts.html.twig')->render([]);
