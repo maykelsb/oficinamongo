030a9628faff8bd176c5fe7147450413dc033bd0
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-08 21:51:59 -0300

    Passando parâmetros da busca para a listagem de posts

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ if (Util::checkRequest()) {
 echo $twig->loadTemplate('posts.html.twig')->render([
     'fm' => Util::getFm(),
     'timeline' => Util::getRepo()->listTimeline(),
-    'posts' => Util::getRepo()->listPosts(),
+    'posts' => Util::getRepo()->listPosts(filter_input_array(INPUT_GET)),
     'tags' => Util::getRepo()->topTags(),
     'shouters' => Util::getRepo()->topShouters()
 ]);
