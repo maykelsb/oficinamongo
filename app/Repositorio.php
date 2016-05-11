1be7a5e6690c04f3b2e0b00166c00a8161aa87a5
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-10 22:40:47 -0300

    Removendo queries de agrupamentos e top10's

--- app/Repositorio.php
+++ app/Repositorio.php
@@ -71,13 +71,7 @@ class Repositorio
      */
     public function topTags()
     {
-        return $this->db->aggregate([
-            ['$project' => ['_id' => 0, 'tags' => 1]],
-            ['$unwind' => '$tags'],
-            ['$group' => ['_id' => '$tags', 'qtd' => ['$sum' => 1]]],
-            ['$project' => ['_id' => 0, 'tag' => '$_id', 'qtd' => 1]],
-            ['$sort' => ['qtd' => -1, 'tag' => 1]]
-        ])->toArray();
+        return [];
     }
 
     /**
@@ -87,105 +81,21 @@ class Repositorio
      */
     public function top5Shouters()
     {
-        return $this->db->aggregate([
-            ['$group' => [ '_id' => '$shouter', 'qtd' => ['$sum' => 1]]],
-            ['$project' => ['_id' => 0, 'shouter' => '$_id', 'qtd' => 1]],
-            ['$sort' => ['qtd' => -1, 'shouter' => 1]],
-            ['$limit' => 5]
-        ])->toArray();
+        return [];
     }
 
     public function listTimeline()
     {
-//        return $this->db->aggregate([
-//            [
-//                '$group' => [
-//                    '_id' => '$when.getMonth()',
-//                    'qtd' => ['$sum' => 1]
-//                ]
-//            ]
-//        ])->toArray();
-
-
-
-
-        return $this->db->aggregate([
-            ['$project' => ['_id' => 0, 'when2' => '$when.toString()']]
-
-
-
-
-//            ['$group' => [
-//                '_id' => ['$dateToString' => ['format' => '%m/%Y', 'date' => '$when']]
-//            ]],
-//            ['$project' => ['_id' => 0, 'when' => '$_id']]
-
-
-//            ['$project' => [
-//                '_id' => 0,
-//                'when' => 1,
-////                'when2' => 'ISODate($when)'
-//            ]]
-//            ['$project' => ['_id' => 0, 'when' => 1]]
-//            ['$project' => ['_id' => 0, 'monyear' => ['$dateToString' => ['format' => '%m/%Y', 'date' => 'ISODate($when)']]]],
-//            [
-//                '$project' => [
-//                    '_id' => [
-//                        'month' => '$when.getMonth()',
-//                        'year' => '$when.getYear()'
-//                    ],
-//
-//                ]
-//            ],
-//            [
-//                '$group' => [
-////                    '_id' => '$monyear',
-////                    '_id' => [
-////                        'month' => ['$month' => '$when'],
-////                        'year' => ['$year' => '$when']
-////                    ],
-//                    '_id' => '$_id',
-//                    'qtd' => ['$sum' => 1]
-//                ]
-//            ],
-//            ['$project' => ['_id' => 0, 'when' => '$_id', 'qtd' => 1]]
-        ])->toArray();
+        return [];
     }
 
     public function top10Pitacados()
     {
-        return $this->db->aggregate([
-            ['$unwind' => ['path' => '$pitacos', 'preserveNullAndEmptyArrays' => true]],
-            ['$group' => [
-                '_id' => [
-                    'id' => '$_id',
-                    'when' => '$when',
-                    'likes' => '$likes',
-                    'shout' => '$shout',
-                    'shouter' => '$shouter'
-                ],
-                'pitacos' => ['$push' => '$pitacos'],
-                'size' => ['$sum' => 1]]],
-            ['$project' => [
-                '_id' => '$_id.id',
-                'when' => '$_id.when',
-                'likes' => '$_id.likes',
-                'pitacos' => '$pitacos',
-                'shout' => '$_id.shout',
-                'shouter' => '$_id.shouter',
-                'size' => '$size'
-                ]
-            ],
-            ['$sort' => ['size' => -1, 'pitacos' => -1, 'when' => -1]],
-            ['$limit' => 10],
-        ])->toArray();
+        return [];
     }
 
     public function top10Likes()
     {
-        return $this->db->query(
-            [],
-            ['limit' => 10, 'sort' => ['likes' => -1, 'when' => -1]]
-        )->toArray();
+        return [];
     }
 }
